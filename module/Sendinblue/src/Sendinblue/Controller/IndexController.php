8f14ba5e38c42dabab43a21869e2717ab81f3559
qnhama <qnhama@DESKTOP-OGI6RIC>
2016-09-19 11:34:39 -0500

    12 sept 2016

--- module/Sendinblue/src/Sendinblue/Controller/IndexController.php
+++ module/Sendinblue/src/Sendinblue/Controller/IndexController.php
@@ -11,7 +11,6 @@ namespace Sendinblue\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
-use Sendinblue\Model\Correo\Mailin;
 
 
 class IndexController extends AbstractActionController
@@ -22,14 +21,6 @@ class IndexController extends AbstractActionController
     }
 
    public function listsAction() {
-        $mailin = new Mailin("https://api.sendinblue.com/v2.0","ghHMXvqERUftLc75");
-        $data = array( "page" => 1,
-        "page_limit" => 2
-      );
- 
-      $resultado = $mailin->get_folders($data);
-      
-       
         return new ViewModel(array("texto"=>"hola mundo desde Zend, soy C&eacute;sar Cancino<br>id=".$resultado["data"]["folders"][0]["id"]));
    }
     
