b4924eeaf36ab01090afc66aecc1dba578272fc6
qnhama <qnhama@DESKTOP-OGI6RIC>
2016-10-31 13:12:40 -0600

    Merge origin/master
    Conflicts:
    	module/Application/src/Application/Model/Correos/CorreoInscripcion.php

--- config/application.config.php
+++ config/application.config.php
@@ -10,6 +10,7 @@ return array(
     'modules' => array(
         'Application',
         'Sendinblue',
+        'Reportes'
     ),
 
     // These are various options for the listeners attached to the ModuleManager
